10a3521eb89774428e5556b7d2874fb3e7b97dce
Geogle <nike522254@gmail.com>
2022-10-05 20:21:42 +0800

    '完成form表單傳遞'

--- generator/views.py
+++ generator/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def password(request):
+    return render(request,'./password.html',{'password':123456})
+
 def index(request):
     print('Hello Django')
 
--- password/urls.py
+++ password/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
+    path('password/',views.password),
     path('',views.index),
     path('admin/', admin.site.urls),
 ]
